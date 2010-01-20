b4d8bd46c76b0ad1ffe83bdc8c682e1d54460ce3
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2010-01-20 21:50:37 +0100

    Some more refactoring.

--- src/main/scala/name/brijest/storm/model/impl/managers/IdleManager.scala
+++
@@ -1,9 +0,0 @@
-package name.brijest.storm.model.impl.managers
-
-
-import name.brijest.storm.model._
-
-
-case object IdleManager extends TimeOnlyManager {
-  def timedAction(mv: ModelView) = None
-}
